11344459fea00453aae77bd5016f38e5541e9390
Dro Sohrabian <dsohrabian@clevelandohio.gov>
2023-12-15 00:13:18 -0500

    Fixed for new API location/format on homepage

--- src/api.py
+++ src/api.py
@@ -1,122 +1,151 @@
 import adafruit_requests as requests
 from config import config
 import time
+
 # Get wifi details and more from a secrets.py file
 
+# Set up parameters
+url_nextconnect = config["api_url"]
+seconds_1day = 24 * 60 * 60  # 24 hours x 60 minutes x 60 seconds
+network_reset = 1  # minutes before starting network
 
-# set up
-url_nextconnect = config['api_url']
-seconds_1day = 24 * 60 * 60 # 24 hours x 60 minutes x 60 seconds
-network_reset = 1 # minutes before starting network
+routes = config["routes"]
 
-# Where the user's customized routes list in config.py is piped into the code
-# The high level function in RealTimeAPI "fetch_predictions" cycles through
-# the "routes" list set below to get the bus stop/route/direction you want
-routes = config['routes']
 
 class RealTimeAPI:
-    def __init__(self, esp_control): # Each API object needs to start with its own ESP object
+    def __init__(self, esp_control):
         self.esp = esp_control
+
     def fetch_predictions(self, routes: list = routes) -> list:
         results = []
         for route in routes:
-            update = self.fetch_route(route['route_name'], route['route_color'], route['params'], route['cutoff'])
-            time.sleep(.5)
+            update = self.fetch_route(
+                route["route_name"],
+                route["route_color"],
+                route["params"],
+                route["cutoff"],
+            )
+            time.sleep(0.5)
             if not update:
                 continue
             results.append(update)
-            print('Fetched 1 trip update.')
-        results = [result for result in results if result.get('arrival', None)]  # filter out empty updates
-        results = sorted(results, key=lambda x: int(x['arrival'].split(',')[0].strip(config['live_char'])))  # sort by the next arrival for each route/dir/stop
+            print("Fetched 1 trip update.")
+        results = [
+            result for result in results if result.get("arrival", None)
+        ]  # filter out empty updates
+        results = sorted(
+            results,
+            key=lambda x: int(x["arrival"].split(",")[0].strip(config["live_char"])),
+        )  # sort by the next arrival for each route/dir/stop
         return results
 
-    def fetch_route(self, route_name: str, route_color: int, route_params: dict, route_cutoff: int) -> dict:
+    def fetch_route(
+        self, route_name: str, route_color: int, route_params: dict, route_cutoff: int
+    ) -> dict:
         payload = route_params
-        hdrs = {'Content-Type': 'application/json',
-         'Accept': 'application/json',
-         'Accept-Encoding': 'gzip, deflate',
-         'Host': 'nextconnect.riderta.com',
-         'Referer': 'http://nextconnect.riderta.com/LiveDepartureTimes',
-         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'
-         }
+        hdrs = {
+            "Content-Type": "application/json",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        }
         while True:
             try:
                 r = requests.post(url_nextconnect, json=payload, headers=hdrs)
                 if r.status_code == 200:
-                    update = r.json()['d']
+                    update = r.json()["d"]
                     r.close()
 
-                    # current time to measure offset from later
+                    # current time to measure offset from
                     nowtime = time.localtime()
                     now_hour = nowtime.tm_hour
                     now_min = nowtime.tm_min
                     now_sec = nowtime.tm_sec
-                    now_ampm = 'am' if now_hour < 12 else 'pm'
+                    now_ampm = "am" if now_hour < 12 else "pm"
 
                     # calculate output time
-                    crossings = update['routeStops'][0]['stops'][0]['crossings'] # next two upcoming trips, can link to config variable
+                    crossings = update["routeStops"][0]["stops"][0][
+                        "crossings"
+                    ]  # next two upcoming trips, can link to config variable
                     if not crossings:  # Make sure cross
                         return None
                     predictions = []
                     for trip in crossings:
-                        if trip['cancelled'] == 'true':
+                        if trip["cancelled"] == "true":
                             continue
-                        time_key = 'pred' if trip['predTime'] else 'sched'
-                        pred_hour, pred_min = map(int, trip[time_key+'Time'].split(':'))
-                        pred_ampm = trip[time_key+'Period']
-                        pred_hour = self.convert24(pred_ampm, pred_hour)  # convert to 24
-
+                        time_key = "pred" if trip["predTime"] else "sched"
+                        pred_hour, pred_min = map(
+                            int, trip[time_key + "Time"].split(":")
+                        )
+                        pred_ampm = trip[time_key + "Period"]
+                        pred_hour = self.convert24(
+                            pred_ampm, pred_hour
+                        )  # convert to 24
                         # convert times to seconds
                         pred_secs = (pred_hour * 60 * 60) + (pred_min * 60)
                         now_secs = (now_hour * 60 * 60) + (now_min * 60) + now_sec
-
                         # final time difference calculation
-                        if pred_ampm == 'am' and now_ampm == 'pm':  # if bus comes next day after midnight
-                            arrival_in = ((seconds_1day - now_secs) + pred_secs) // 60  # add remaining time to prediction time as minutes
+                        if (
+                            pred_ampm == "am" and now_ampm == "pm"
+                        ):  # if bus comes next day after midnight
+                            arrival_in = (
+                                (seconds_1day - now_secs) + pred_secs
+                            ) // 60  # add remaining time to prediction time as minutes
                         else:
-                            arrival_in = (pred_secs - now_secs) // 60  # otherwise subtract prediction from current time as minutes
-                        arrival_in = str(arrival_in-1)
-                        if time_key == 'sched' and config['not_live_flag']:
-                            arrival_in += config['live_char']  # add flag character when update is static and live GPS based
+                            arrival_in = (
+                                pred_secs - now_secs
+                            ) // 60  # otherwise subtract prediction from current time as minutes
+                        arrival_in = str(arrival_in - 1)
+                        if time_key == "sched" and config["not_live_flag"]:
+                            arrival_in += config[
+                                "live_char"
+                            ]  # add flag character when update is static and live GPS based
                         predictions.append(arrival_in)
 
                     # filter out all before our cutoff time of choice
-                    predictions = list(filter(lambda x: int(x.strip(config['live_char'])) >= route_cutoff, predictions))
+                    predictions = list(
+                        filter(
+                            lambda x: int(x.strip(config["live_char"])) >= route_cutoff,
+                            predictions,
+                        )
+                    )
                     # grab next 2 departs
                     last_2 = predictions[:2]
-                    prediction = ','.join(last_2) if predictions else None  # output 2
+                    prediction = ",".join(last_2) if predictions else None  # output 2
 
-                    # final output of dict
-                    destination = crossings[0]['destination']
-                    tokens = destination.split(' ')
-                    if tokens[0] in ["Red", "Green", "Blue"]:
-                        rail_color = tokens[0]
-                        destination = destination.split(f'{rail_color} Line')[1].strip()
+                    # FINAL API OUTPUT
+                    destination = crossings[0]["destination"]
+                    # Process destination info if provided. After Vontas OnRoute update, GCRTA's API doesn't broadcast destination info yet ("Tower City, Westgate")
+                    if destination:
+                        tokens = destination.split(" ")
+                        if tokens[0] in ["Red", "Green", "Blue"]:
+                            rail_color = tokens[0]
+                            destination = destination.split(f"{rail_color} Line")[
+                                1
+                            ].strip()
+                        else:
+                            destination = " ".join(tokens[1:]).strip("- ")
                     else:
-                        destination = ' '.join(tokens[1:]).strip('- ')
-
+                        destination = None
                     return {
-                        'line_color': route_color, # output 1
-                        'route_name': str(route_name), # output 2
-                        'destination': '',  # output 3, coded to be off by preference, too busy on board
-                        'arrival': prediction,  # output 4
-
+                        "line_color": route_color,  # output 1
+                        "route_name": str(route_name),  # output 2
+                        "destination": "",  # output 3, hard-coded to be empty until destination returns
+                        "arrival": prediction,  # output 4
                     }
                 else:
-                    print('Request status code not successful')
+                    print("Request status code not successful")
                     r = None
                     return None
             except Exception as e:
-                print(f"Failed to get data due to {e}. Resetting ESP")
+                print(f"Failed to get data due to {e}.Resetting ESP")
                 self.esp.reset()
                 self.esp.connect_AP(config["wifi_ssid"], config["wifi_password"])
 
     # helper, converts API's 12 hour time to 24 hour so calculations can be made
     @staticmethod
-    def convert24(period,hour):
-        if period == 'pm' and hour != 12:
+    def convert24(period, hour):
+        if period == "pm" and hour != 12:
             return hour + 12
-        if period == 'am' and hour == 12:
+        if period == "am" and hour == 12:
             return 0
         else:
             return hour
--- src/config.py
+++ src/config.py
@@ -10,27 +10,67 @@ key in each route.
 
 # These are dicts of POST payload data for specific transit stops. The transit authority API uses this structure.
 
-route_1 = {'route_name': '22',
-           'route_color': 0x0011aa,
-           'params': {'routeID': 139, 'directionID': 3, 'stopID': 13363, 'tpID': 0, 'useArrivalTimes': 'false'},
-           'cutoff': 5}
-route_2 = {'route_name': '45',
-           'route_color': 0xa81995,
-           'params': {'routeID': 60, 'directionID': 5, 'stopID': 12925, 'tpID': 0, 'useArrivalTimes': 'false'},
-           'cutoff': 5}
-route_3 = {'route_name': '26',
-           'route_color': 0x22cf19,
-           'params': {'routeID': 165, 'directionID': 3, 'stopID': 1342, 'tpID': 0, 'useArrivalTimes': 'false'},
-           'cutoff': 5}
-route_4 = {'route_name': '71',
-           'route_color': 0xde4e00,
-           'params': {'routeID': 193, 'directionID': 5, 'stopID': 1342, 'tpID': 0, 'useArrivalTimes': 'false'},
-           'cutoff': 5}
-route_5 = {'route_name': 'RL-E',
-           'route_color': 0xdd0000,
-           'params': {'routeID': 147, 'directionID': 3, 'stopID': 16125, 'tpID': 919, 'useArrivalTimes': 'false'},
-           'cutoff': 15}
-           
+route_1 = {
+    "route_name": "22-E",
+    "route_color": 0x0011AA,
+    "params": {
+        "routeID": 139,
+        "directionID": 3,
+        "stopID": 13363,
+        "tpID": 0,
+        "useArrivalTimes": "false",
+    },
+    "cutoff": 5,
+}
+route_2 = {
+    "route_name": "45-E",
+    "route_color": 0xA81995,
+    "params": {
+        "routeID": 60,
+        "directionID": 5,
+        "stopID": 12925,
+        "tpID": 0,
+        "useArrivalTimes": "false",
+    },
+    "cutoff": 5,
+}
+route_3 = {
+    "route_name": "26-E",
+    "route_color": 0x22CF19,
+    "params": {
+        "routeID": 165,
+        "directionID": 3,
+        "stopID": 1342,
+        "tpID": 0,
+        "useArrivalTimes": "false",
+    },
+    "cutoff": 5,
+}
+route_4 = {
+    "route_name": "71-E",
+    "route_color": 0xDE4E00,
+    "params": {
+        "routeID": 193,
+        "directionID": 5,
+        "stopID": 1342,
+        "tpID": 0,
+        "useArrivalTimes": "false",
+    },
+    "cutoff": 5,
+}
+route_5 = {
+    "route_name": "RL-E",
+    "route_color": 0xDD0000,
+    "params": {
+        "routeID": 147,
+        "directionID": 3,
+        "stopID": 16125,
+        "tpID": 919,
+        "useArrivalTimes": "false",
+    },
+    "cutoff": 15,
+}
+
 # you can feasibly add as many as you like but the board can only show 3 at a time and the network requests take time
 
 routes = [route_1, route_2, route_3, route_4, route_5]
@@ -40,43 +80,44 @@ config = {
     # Network Configuration #
     #########################
     # Wifi and aio credentials
-    'wifi_ssid': secrets['ssid'],
-    'wifi_password': secrets['password'],
-    'aio_username': secrets['aio_username'],
-    'aio_key': secrets['aio_key'],
-
-    'timezone': "America/New_York",
-    'api_url': 'http://nextconnect.riderta.com/Arrivals.aspx/getStopTimes',  # the transit authority API
-    'refresh_interval': 30,  # GCRTA feed seems to update every 30 seconds
-    'cutoff_min': 4,  # minimum minutes til arrival to remove buses you won't make it to
-
+    "wifi_ssid": secrets["ssid"],
+    "wifi_password": secrets["password"],
+    "aio_username": secrets["aio_username"],
+    "aio_key": secrets["aio_key"],
+    # Time settings
+    "timezone": "America/New_York",
+    # API and fetch settings
+    "api_url": "https://webwatch.gcrta.vontascloud.com/TMWebWatch/Arrivals.aspx/getStopTimes",  # the transit authority API
+    "refresh_interval": 30,  # GCRTA feed seems to update every 30 seconds
+    "cutoff_min": 4,  # minimum minutes til arrival to remove buses you won't make it to
     # Display Settings
-    'matrix_width': 64,
-    'num_routes_display': 3,
-    'font': bitmap_font.load_font('lib/5x7.bdf'),
-    'time_font': bitmap_font.load_font('lib/4x6.bdf'),
-
-    'character_width': 5,
-    'character_height': 7,
-    'text_padding': 3,
-    'text_color': 0x552200,
-    'dest_color': 0xFF7500,
-    'not_live_flag': False,  # adds character after minute if update is scheduled and not live (static)
-    'live_char': '.',  # character to use to flag if a static update
-
-    'loading_route_text': '--',
-    'loading_destination_text': '---',
-    'loading_min_text': '---',
-    'loading_line_color': 0xFF00FF,  # purple
-    'loading_time_msg': 'ride rta',
-
-    'heading_text': 'RTE    DEPARTING',
-    'heading_color': 0x550000,  # red
-
-    'train_line_height': 6,
-    'train_line_width': 2,
-
-    'min_label_characters': 5,
-
-    'routes': routes  # fed to the API
+    "matrix_width": 64,
+    "num_routes_display": 3,
+    "font": bitmap_font.load_font("lib/5x7.bdf"),
+    "time_font": bitmap_font.load_font("lib/4x6.bdf"),
+    # Core text settings
+    "character_width": 5,
+    "character_height": 7,
+    "text_padding": 3,
+    "text_color": 0x552200,
+    "dest_color": 0xFF7500,
+    "not_live_flag": False,  # adds character after minute if update is scheduled and not live (static)
+    "live_char": ".",  # character to use to flag if a static update
+    # Loading indicator and text settings
+    "loading_route_text": "--",
+    "loading_destination_text": "---",
+    "loading_min_text": "---",
+    "loading_line_color": 0xFF00FF,  # purple
+    "loading_time_msg": "ride rta",
+    # Heading text
+    "heading_text": "ROUTE    MINUTES",
+    "heading_color": 0x00008B,  # red
+    # Clock text
+    "clock_color": 0x00008B,
+    # Train color bar settings
+    "train_line_height": 6,
+    "train_line_width": 2,
+    "min_label_characters": 5,
+    # Five routes above stored under this key for API use
+    "routes": routes
 }
